cb12ee70fa1cc3c554cf0bb0f8271ba76fd67e0f
hilmyys <hilmiyr123@gmail.com>
2025-11-09 11:05:19 +0700

    done

---
+++ Latihan/OOP/Rumah.php
@@ -0,0 +1,18 @@
+<?php
+
+class Rumah {
+  
+
+
+  public $Warna = "Putih";
+  public $jumlahKamar = 4;
+  public $alamat = "Jln. Pasundan No. 1";
+}
+
+$rumahSaya =  new Rumah();
+
+$rumahTetangga =  new Rumah();
+
+
+
+?>
