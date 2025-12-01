bcf24114bd9675e2a10c431f773a2e5bb645cc4b
hilmyys <hilmiyr123@gmail.com>
2025-12-01 15:25:34 +0700

    UPDATE

---
+++ Latihan/Pertemuan3/latihan/latihan1/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    {{-- Mengirim data $title ke layout --}}
+    <x-slot:title>
+        Home Page
+    </x-slot:title>
+
+    {{-- Ini adalah konten utama yang akan masuk ke $slot di layout --}}
+    <h1>Halaman Home</h1>
+    <p>Selamat datang di website saya.</p>
+</x-layout>
